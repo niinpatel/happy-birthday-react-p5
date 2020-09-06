e16ac1dd311702b8046305c114c78102bb4141b8
Nitin Patel <nitinpatel278@gmail.com>
2020-09-06 12:14:57 +0530

    add comments explaining textToPoint args

--- src/sketch/sketch.js
+++ src/sketch/sketch.js
@@ -18,10 +18,15 @@ export default (sketch) => {
 
   const fillDots = (width, height) => {
     dots = [];
+    // A dirty hack to make it work on both desktop and mobile phones
     if (width > height) {
       // console.log('desktop view')
       let firstWord = 'Happy Birthday'
       let secondWord = 'Trisha'
+
+      // ref: https://p5js.org/reference/#/p5.Font/textToPoints
+      // I just tweaked these numbers until it "looked right", mainly trial and error :D 
+      // The numbers would be different for different text, any suggestions about better way to do this are welcome :) 
       firstWordPoints = font.textToPoints(firstWord, width * 0.04, height * 0.33, width * 0.12)
       secondWordPoints = font.textToPoints(secondWord, width * 0.05, height * 0.83, width * 0.3);
     } else {
@@ -29,6 +34,7 @@ export default (sketch) => {
       let firstWord = 'Happy'
       let secondWord = 'Birthday';
       let thirdWord = 'Trisha'
+
       firstWordPoints = font.textToPoints(firstWord, width * 0.01, height * 0.3, width * 0.3);
       secondWordPoints = font.textToPoints(secondWord, width * 0.01, height * 0.5, width * 0.25);
       thirdWordPoints = font.textToPoints(thirdWord, width * 0.01, height * 0.7, width * 0.32);
