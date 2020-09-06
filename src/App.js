b22b9c7ede836d8e931ea3aff65e9f17c20dd17c
Nitin Patel <nitinpatel278@gmail.com>
2020-09-06 12:03:48 +0530

    use functional components

--- src/App.js
+++ src/App.js
@@ -3,18 +3,16 @@ import './App.css';
 import sketch from './sketch/sketch';
 import p5 from 'p5';
 
-class App extends React.Component { // TODO: refactor into functional component
-  componentDidMount() {
-    new p5(sketch, this.canvas);
-  }
+const App = () => {
+  const canvasRef = React.useRef(null)
 
-  render() {
-    return (
-      <div className="main">
-        <div id="canvas-parent" ref={canvas => (this.canvas = canvas)} />
-      </div >
-    );
-  }
+  React.useEffect(() => { new p5(sketch, canvasRef.current); })
+
+  return (
+    <div className="main">
+      <div id="canvas-parent" ref={canvasRef} />
+    </div >
+  )
 }
 
 export default App;
