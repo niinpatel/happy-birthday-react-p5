c57fbbf389aec4889f8e85b6eca564e9ab2a4d37
Nitin Patel <nitinpatel278@gmail.com>
2020-09-06 12:40:38 +0530

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
-import sketch from './sketch/sketch';
+import sketch from './sketch';
 import p5 from 'p5';
+
+const TEXT = 'Happy Birthday Trisha';
 
 const App = () => {
   const canvasRef = React.useRef(null)
 
-  React.useEffect(() => { new p5(sketch(canvasRef.current), canvasRef.current); })
+  React.useEffect(() => {
+    const canvasDivElement = canvasRef.current;
+    new p5(sketch(canvasDivElement, TEXT), canvasDivElement);
+  })
 
   return (
     <div className="main">
--- src/sketch/sketch.js
+++ src/sketch/sketch.js
@@ -4,7 +4,7 @@ import fontFile from './AvenirNextLTPro-Demi.otf';
 const defaultFrameRate = 30; // low framerate to avoid too much strain on mobile phones
 
 
-export default (parent) => (sketch) => {
+export default (parent, text) => (sketch) => {
   let font;
   let dots;
   let firstWordPoints;
@@ -17,22 +17,19 @@ export default (parent) => (sketch) => {
 
   const fillDots = (width, height) => {
     dots = [];
+    let [firstWord, secondWord, thirdWord] = text.split(' ') // Assumption that text is just 3 words, no more, no less
+
     // A dirty hack to make it work on both desktop and mobile phones
     if (width > height) {
       // console.log('desktop view')
-      let firstWord = 'Happy Birthday'
-      let secondWord = 'Trisha'
 
       // ref: https://p5js.org/reference/#/p5.Font/textToPoints
       // I just tweaked these numbers until it "looked right", mainly trial and error :D 
       // The numbers would be different for different text, any suggestions about better way to do this are welcome :) 
-      firstWordPoints = font.textToPoints(firstWord, width * 0.04, height * 0.33, width * 0.12)
-      secondWordPoints = font.textToPoints(secondWord, width * 0.05, height * 0.83, width * 0.3);
+      firstWordPoints = font.textToPoints(`${firstWord} ${secondWord}`, width * 0.04, height * 0.33, width * 0.12)
+      secondWordPoints = font.textToPoints(thirdWord, width * 0.05, height * 0.83, width * 0.3);
     } else {
       // console.log('in mobile view')
-      let firstWord = 'Happy'
-      let secondWord = 'Birthday';
-      let thirdWord = 'Trisha'
 
       firstWordPoints = font.textToPoints(firstWord, width * 0.01, height * 0.3, width * 0.3);
       secondWordPoints = font.textToPoints(secondWord, width * 0.01, height * 0.5, width * 0.25);
@@ -46,7 +43,6 @@ export default (parent) => (sketch) => {
     secondWordPoints.forEach((point) => {
       dots.push(new Dot(point.x, point.y, sketch));
     });
-
 
     thirdWordPoints.forEach((point) => {
       dots.push(new Dot(point.x, point.y, sketch));
