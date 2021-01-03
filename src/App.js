4d1729028f6f222f7e7572fd21e390d8841864df
Gautham Prabhu M <71874814+GauthamPrabhuM@users.noreply.github.com>
2021-01-03 11:51:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import sketch from './sketch';
 import p5 from 'p5';
 
-const TEXT = 'Happy Birthday Trisha';
+const TEXT = 'Happy Birthday SaiShree';
 
 const App = () => {
   const canvasRef = React.useRef(null)
